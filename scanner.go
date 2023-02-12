def92d188a1dd05d6b9d004ef40f4bded1458884
Konstantin <57292616+mcei@users.noreply.github.com>
2023-02-12 19:27:56 +0700

    Implemented

---
+++ scanner.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"time"
+)
+
+var timeout = 1 * time.Second
+
+func scan (host string, port int) {
+	address := fmt.Sprintf("%s:%d", host, port)
+
+	connection, err := net.DialTimeout("tcp", address, timeout)
+
+	if err != nil{
+		return
+	}
+	
+	fmt.Println("Remote address:", connection.RemoteAddr().String())
+	fmt.Printf("\tport %d is open\n", port)
+
+	connection.Close()
+}
+
+func main() {
+
+	var host string
+	var port int
+
+	flag.StringVar(&host, "host", "", "ip or root domain")
+	flag.IntVar(&port, "port", 80, "port")
+
+	flag.Parse()
+	
+	scan(host, port)
+}
